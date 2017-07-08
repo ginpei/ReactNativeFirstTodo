f331b0635154f5f8624733d591569d6bb0e057a5
ginpei <ginpei@gmail.com>
2017-07-07 19:30:57 -0700

    list view

---
+++ app/components/TodoList/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	ListView,
+} from 'react-native'
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			tasks: [
+				{
+					id: 1,
+					title: 'Buy milk',
+					detail: '',
+				},
+				{
+					id: 2,
+					title: 'Buy chicken tigh',
+					detail: '',
+				},
+				{
+					id: 3,
+					title: 'Pay forward',
+					detail: 'Make the world peaceful',
+				},
+			]
+		}
+
+		const ds = new ListView.DataSource({ 
+			rowHasChanged: (d1, d2) => d1.title !== d2.title || d1.detail !== d2.detail
+		})
+		this.state.taskDataSource = ds.cloneWithRows(this.state.tasks)
+	}
+
+	render() {
+		return (
+			<View>
+				<ListView
+					dataSource={this.state.taskDataSource}
+					renderRow={(d) => <Text key={d.id}>{d.title}</Text> }
+					/>
+			</View>
+			)
+	}
+
+	item_onclick(task) {
+		console.log('# hoge')
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react'
-import {
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native'
+import TodoList from '../components/TodoList'
 
 export default class Home extends Component {
 	render() {
 		return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello React Native World!</Text>
-        <Text style={styles.instructions}>I'm gonna create a great app!</Text>
-      </View>
+			<TodoList />
 			)
 	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
